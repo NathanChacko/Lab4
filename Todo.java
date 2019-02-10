9de26aa9413fe2225533b8b3773868db5d1eb02d
NathanChacko <47375381+NathanChacko@users.noreply.github.com>
2019-02-09 23:27:53 -0600

    Committing initial code changes for lab4

---
+++ Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 
+Last Modified_______: 2019-02-09
+OU ID_______________: 113361221
+OU EMAIL____________: nathan.t.chacko-1@ou.edu
+
+Tasks:
+1. Equipment
+2. Equipment Test
+3. Test Runner
+4. Utility
+5. Zoo
+6. Zoo Test
+
+Estimated Time (minutes):
+1. 20
+2. 15
+3. 30
+4. 20
+5. 20
+6. 15
+
+Actual Time (minutes):
+1. 20
+2. 15
+3. 30
+4. 20
+5. 20
+6. 15
+*/}
